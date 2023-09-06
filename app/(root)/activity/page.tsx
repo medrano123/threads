8c78b04ca9180481cef0831a9620e05d21b6e8e8
Giovanni Medrano <medranoector@yahoo.com>
2023-09-06 13:59:19 -0500

    test

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
 	return (
 		<section>
 			<h1 className="text-heading2-bold text-light-1 mb-10">
-				Activity
+				Test
 			</h1>
 		</section>
 	)
