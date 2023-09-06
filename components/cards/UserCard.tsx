b4d15b31ced153efce71a5d2dfaa048792f51149
Giovanni Medrano <medranoector@yahoo.com>
2023-09-06 13:46:21 -0500

    activity

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -15,7 +15,6 @@ interface Props {
 
 function UserCard({ id, name, username, imgUrl, personType }: Props) {
     const router = useRouter();
-
     const isCommunity = personType === "Community";
 
     return (
@@ -31,19 +30,24 @@ function UserCard({ id, name, username, imgUrl, personType }: Props) {
                 </div>
 
                 <div className='flex-1 text-ellipsis'>
-                    <h4 className='text-base-semibold text-light-1'>{name}</h4>
-                    <p className='text-small-medium text-gray-1'>@{username}</p>
+                    <h4 className='text-base-semibold text-light-1'>
+                        {name}
+                    </h4>
+                    <p className='text-small-medium text-gray-1'>
+                        @{username}
+                    </p>
                 </div>
             </div>
+
 
             <Button
                 className='h-auto min-w-[74px] rounded-lg bg-primary-500 text-[12px] text-light-1'
                 onClick={() => {
-                if (isCommunity) {
-                    router.push(`/communities/${id}`);
-                } else {
-                    router.push(`/profile/${id}`);
-                }
+                    if (isCommunity) {
+                        router.push(`/communities/${id}`);
+                    } else {
+                        router.push(`/profile/${id}`);
+                    }
                 }}
             >
                 View
