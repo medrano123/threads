edb6ca0e814f06dc72b66601c8202afd6ea194f9
Giovanni Medrano <medranoector@yahoo.com>
2023-09-07 09:54:05 -0500

    deploying for clerk community implementation

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-	return (
-		<div className="flex items-center justify-center h-screen">
-			<SignUp />;
+  	return (
+		<div className="flex items-center justify-center h-screen bg-dark-1">
+			<SignUp />
 		</div>
-	)
+  	)
 }
